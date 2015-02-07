a8eb6987c06678681bdf128afbef8891b1926757
alexandermonsanto <alexandermonsanto@gmail.com>
2015-02-07 14:29:28 -0800

    calculator controller fmv

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,5 +1,12 @@
 class CalculatorsController < ApplicationController
   def index
+    # source: http://www.huduser.org/portal/datasets/fmr/fmrs/FY2015_code/2015state_summary.odn
+    var fmr_eff_bd = 972
+    var fmr_one_bd = 1150
+    var fmr_wo_bd = 1415
+    var fmr_three_bd = 2085
+    var fmr_four_bd = 2506
+
   end
 
   def show
