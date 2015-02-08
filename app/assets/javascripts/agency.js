c7841b09731a3112cb162e805445a85448a0a2f8
alexandermonsanto <alexandermonsanto@gmail.com>
2015-02-07 19:14:12 -0800

    misc update

--- app/assets/javascripts/agency.js
+++ app/assets/javascripts/agency.js
@@ -16,9 +16,9 @@ $(function() {
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+// $('#page-top').scrollspy({
+//     target: '#navbar-fixed-top'
+// })
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
