6eb68d0ed38d98e4a43545e0481efbf5762b629e
DEVDESK <swoolsey@devdesk.net>
2015-02-08 09:44:05 -0800

    map styled, zoom fixed

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -1,18 +1,20 @@
-  var mapStyle = [
-  {
-    "featureType": "water",
-    "stylers": [
-      { "color": "#B1BDD7" }
-    ];
 
-  var drawMap = function(mapOptions){
-    var handler = Gmaps.build('Google');
-      handler.buildMap({
-      internal: {id: 'custom_style'},
-      provider: {
-        zoom:      15,
-        center:    new google.maps.LatLng(47.6097, -122.3331),
-        styles:    mapStyle
-      }
-    })
-  };
+  // var mapStyle = [
+  // {
+  //   "featureType": "water",
+  //   "stylers": [
+  //     { "color": "#B1BDD7" }
+  //   ]
+  // }]
+
+  // var drawMap = function(mapOptions){
+  //   var handler = Gmaps.build('Google');
+  //     handler.buildMap({
+  //     internal: {id: 'custom_style'},
+  //     provider: {
+  //       zoom:      15,
+  //       center:    new google.maps.LatLng(47.6097, -122.3331),
+  //       styles:    mapStyle
+  //     }
+  //   })
+  // };
