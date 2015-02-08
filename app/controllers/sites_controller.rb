9d10729f844ebd0af682c0f1bd04e2550fbc9cc8
DEVDESK <swoolsey@devdesk.net>
2015-02-08 05:07:34 -0800

    worked on api request data from zillow

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -3,38 +3,40 @@ class SitesController < ApplicationController
 
     def map
         input_url = "http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm?zws-id=X1-ZWz1az100jazgr_82rpe&zpid=48749425"
-        doc = Nokogiri::HTML(open(input_url)) do |config|
-        config.options = Nokogiri::XML::ParseOptions::STRICT | Nokogiri::XML::ParseOptions::NONET
+        @doc = Nokogiri::HTML(open(input_url)) do |config|
+        config.options = Nokogiri::XML::ParseOptions::STRICT
         end
-        doc.xpath('//response').each do |path|
-            @latitude       = path.at_xpath('//address//latitude').content
-            @longitude      = path.at_xpath('//address//longitude').content
-            # img 
-            @imgsrc         = path.at_xpath('//images//image//url').content
+        @doc.xpath('//response').each do |path|
+            @latitude       = path.at_xpath('//address/latitude').content unless path.at_xpath('//address/latitude').nil?
+            @longitude      = path.at_xpath('//address/longitude').content unless path.at_xpath('//address/longitude').nil?
+            # img
+            @imgsrc         = path.at_xpath('//images/image/url').content unless path.at_xpath('//images/image/url').nil?
             #basic info
-            @address        = path.at_xpath('//address//street').content+" "+
-                              path.at_xpath('//address//city').content+", "+
-                              path.at_xpath('//address//state').content+" "+
-                              path.at_xpath('//address//zipcode').content
-            @neighborhood   = path.at_xpath('//neighborhood').content
-            # @schooldistrict = path.at_xpath('//schoolDistrict').content
-
+            @address        = path.at_xpath('//address/street').content+" "+
+                              path.at_xpath('//address/city').content+", "+
+                              path.at_xpath('//address/state').content+" "+
+                              path.at_xpath('//address/zipcode').content unless (path.at_xpath('//address/street').content+" "+
+                              path.at_xpath('//address/city').content+", "+
+                              path.at_xpath('//address/state').content+" "+
+                              path.at_xpath('//address/zipcode')).nil?
+            @neighborhood   = path.at_xpath('neighborhood').content unless path.at_xpath('neighborhood').nil?
+            @schoolDistrict = path.at_xpath('schooldistrict').content unless path.at_xpath('schooldistrict').nil?
             # #first table
-            # @roomtype       = path.at_xpath('//editedFacts//rooms').content
-            # @sqft           = path.at_xpath('//editedFacts//finishedSqFt').content
-            # @lotsqft        = path.at_xpath('//editedFacts//lotSizeSqFt').content
-            # @familysize     = path.at_xpath('//editedFacts//useCode').content
-            # @yearbuilt      = path.at_xpath('//editedFacts//yearBuilt').content
-            # @roof           = path.at_xpath('//editedFacts//roof').content
-            # @heatingsystem  = path.at_xpath('//editedFacts//heatingSystem').content
+            @roomtype       = path.at_xpath('//editedfacts/rooms').content unless path.at_xpath('//editedfacts/rooms').nil?
+            @sqft           = path.at_xpath('//editedfacts/finishedsqft').content unless path.at_xpath('//editedfacts/finishedsqft').nil?
+            @lotsqft        = path.at_xpath('//editedfacts/lotsizesqft').content unless path.at_xpath('//editedfacts/lotsizesqft').nil?
+            @familysize     = path.at_xpath('//editedfacts/usecode').content unless path.at_xpath('//editedfacts/usecode').nil?
+            @yearbuilt      = path.at_xpath('//editedfacts/yearbuilt').content unless path.at_xpath('//editedfacts/yearbuilt').nil?
+            @roof           = path.at_xpath('//editedfacts/roof').content unless path.at_xpath('//editedfacts/roof').nil?
+            @heatingsystem  = path.at_xpath('//editedfacts/heatingsystem').content unless path.at_xpath('//editedfacts/heatingsystem').nil?
 
             # #second table
-            # @bedroom        = path.at_xpath('//editedFacts//bedrooms').content
-            # @bathrooms      = path.at_xpath('//editedFacts//bathrooms').content
-            # @lastsolddate   = path.at_xpath('//editedFacts//lastSoldDate').content
-            # @lastsoldprice  = path.at_xpath('//editedFacts//lastSoldPrice').content
-            # @numoffloors    = path.at_xpath('//localRealEstate//numFloors').content
-            # @yearupdated    = path.at_xpath('//localRealEstate//yearUpdated').content
+            @bedrooms       = path.at_xpath('//editedfacts/bedrooms').content.to_i.floor unless path.at_xpath('//editedfacts/bedrooms').nil?
+            @bathrooms      = path.at_xpath('//editedfacts/bathrooms').content.to_i unless path.at_xpath('//editedfacts/bathrooms').nil?
+            @lastsolddate   = path.at_xpath('//editedfacts/lastsolddate').content unless path.at_xpath('//editedfacts/lastsolddate').nil?
+            @lastsoldprice  = path.at_xpath('//editedfacts/lastsoldprice').content unless path.at_xpath('//editedfacts/lastsoldprice').nil?
+            @numoffloors    = path.at_xpath('//editedfacts/numfloors').content unless path.at_xpath('//editedfacts/numfloors').nil?
+            @yearupdated    = path.at_xpath('//editedfacts/yearupdated').content unless path.at_xpath('//editedfacts/yearupdated').nil?
         end
     end
 end
